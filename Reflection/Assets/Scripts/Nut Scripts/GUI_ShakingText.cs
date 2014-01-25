a7c84cf33040e1996911ea04b7061fabe97359c6
Pakkanut Mee-Im <nut@arkavis.com>
2014-01-25 13:38:26 +0700

    Update startmenu and loading menu.

---
+++ Reflection/Assets/Scripts/Nut Scripts/GUI_ShakingText.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_ShakingText : MonoBehaviour {
+
+
+    private Vector3 originPosition;
+    private float shake_decay;
+    private bool _isEndless;
+    private float permanent_intensity;
+    [HideInInspector] public float shake_intensity;
+
+    // Use this for initialization
+    void Start()
+    {
+        originPosition = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            if (shake_intensity > 0)
+            {
+                transform.localPosition = originPosition + Random.insideUnitSphere * shake_intensity;
+                shake_intensity -= shake_decay;
+            } 
+            else
+            {
+                if(_isEndless)
+                {
+                    shake_intensity = permanent_intensity;
+                }
+            }
+
+    }
+
+    public void Shake(float intensity, float decay ,bool _endless)
+    {
+        _isEndless = _endless;
+        if (_isEndless)
+        {
+            permanent_intensity = intensity;
+        }
+
+        shake_intensity = intensity;
+        shake_decay = decay;
+    }
+
+    public void StopShake()
+    {
+        _isEndless = false;
+        shake_intensity = 0;
+    }
+}
---
+++ Reflection/Assets/Scripts/Nut Scripts/Gui_Loadingscreen.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gui_Loadingscreen : MonoBehaviour {
+
+    public static Gui_Loadingscreen instance;
+
+    public Transform panel;
+    public UILabel loading_Text;
+    public bool stop_test;
+    public float my_intensity;
+    void ShakingText()
+    {
+        (loading_Text.GetComponent("GUI_ShakingText") as GUI_ShakingText).Shake(my_intensity, .75f, true);
+    }
+
+    void StopShakingText()
+    {
+        (loading_Text.GetComponent("GUI_ShakingText") as GUI_ShakingText).StopShake();
+    }
+
+    public void Show()
+    {
+        panel.gameObject.SetActive(true);
+        ShakingText();
+    }
+
+    public void Hide()
+    {
+        StopShakingText();
+        panel.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        instance = this;
+        panel.gameObject.SetActive(false);
+    }
+
+}
