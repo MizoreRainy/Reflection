c9265921d63641a045ab756c390bf4b33404b6c8
Konstantin K <konstantin.koss@gmail.com>
2014-01-25 17:08:58 +0700

    Move Sounds and create Trigger Scripts

---
+++ Reflection/Assets/Scripts/Triggers/PlaySound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+	public AudioSource sound1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnTriggerEnter(Collider coll) {
+		if (coll.collider.gameObject.tag == "Player") {
+			Debug.Log ("ok");
+			sound1.Play();
+		}
+	}
+}
