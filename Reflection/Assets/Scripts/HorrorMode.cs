a4df54b5f8d26649ce304afe342ec63fd3261b72
MarcinMajchrzak <marcin@arkavis.com>
2014-01-25 13:38:53 +0700

    new assets

---
+++ Reflection/Assets/Scripts/HorrorMode.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorrorMode : MonoBehaviour {
+
+	public Animation horror;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if(Input.GetKeyDown(KeyCode.O)){
+			HorrorModeStatus(true);
+		}
+
+		if(Input.GetKeyDown(KeyCode.P)){
+			HorrorModeStatus(false);
+		}
+	}
+
+	public void HorrorModeStatus(bool status){
+		if(status){
+			horror.Play("horrorOn");
+		}else{
+			horror.Play("horrorOff");
+		}
+	}
+}
